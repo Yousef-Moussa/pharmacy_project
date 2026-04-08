8322aab67c5c2930367d63b1ac7c7e88a68b1c87
Yousef Ali Moussa <yousefmoussa2003@yahoo.com>
2026-04-08 20:44:13 +0800

    inventory making

---
+++ src/modules/inventory/InventoryService.js
@@ -0,0 +1,17 @@
+
+
+export class Inventory{
+    constructor(productID, name, price, stock, expiryDate, category){
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.expiryDate = expiryDate;
+        this.category = category;
+        this.dateAdded = new Date();
+    }
+getAll(){
+    
+}
+}
+
