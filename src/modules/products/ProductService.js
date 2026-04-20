066d49ec63b2e14291411c8130a4eec7fb32201e
Yousef Ali Moussa <yousefmoussa2003@yahoo.com>
2026-04-21 07:15:26 +0800

    try to figure the relations out

--- src/modules/products/ProductService.js
+++ src/modules/products/ProductService.js
@@ -1,16 +1,8 @@
 
-
-export class Inventory{
-    constructor(productID, name, price, stock, expiryDate, category){
-        this.productID = productID;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.expiryDate = expiryDate;
-        this.category = category;
-        this.dateAdded = new Date();
-    }
+// import { Product } from "C:\Users\12111\Desktop\Pharmacy_project\src\utils\storage.js"
 
 
-}
+const product1 = new Product(321,"Med1",30);
+
+console.log(product1);
 
---
+++ src/utils/storage.js
@@ -0,0 +1,20 @@
+
+//product
+export class Product{
+
+createProduct(productID, name, quantity){
+    return{
+        productID,
+        name,
+        quantity
+    };
+
+
+}
+
+}
+//name
+
+
+
+// admin
