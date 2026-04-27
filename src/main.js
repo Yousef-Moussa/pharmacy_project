95d8b686774a9dc45762ee11a9bd9f299459dde8
Yousef Ali Moussa <yousefmoussa2003@yahoo.com>
2026-04-27 14:45:19 +0800

    linking html to database brainstorming

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import * as productView from './modules/users/UserService.js';
 
 
-
-console.log("hello world");
+export function myElements(){
+const newUsername = document.getElementById("registerUsername");
+const newPassword = document.getElementById("registerPassword");
+const signUpBtn = document.getElementById("signUpBtn");
+}
+console.log(productView.printName());
---
+++ src/modules/users/UserService.js
@@ -0,0 +1,16 @@
+
+
+import * as main from '../../main.js'
+let username = "";
+let password = "";
+
+main.myElements.signUpBtn.onclick =  function(){
+    username = newUsername.value;
+    password = newPassword.value;
+}
+
+console.log(main.myElements.newUsername.value);
+console.log(username)
+
+
+
